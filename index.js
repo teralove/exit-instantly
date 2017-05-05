f5f46e7e2ca52a55a313c7e635a269ad3304f9d7
teralove <terascripts@gmail.com>
2017-05-05 11:57:40 -0400

    cleaned up dispatch

--- index.js
+++ index.js
@@ -3,12 +3,10 @@
 module.exports = function ExitInstantly(dispatch) {
 				
     dispatch.hook('S_PREPARE_EXIT', 1, function(event) {
-		//S_EXIT has 2 int32's, the 2nd one has a value of 16. 
-		//Not sure of the purpose, but it still works without it.
-		dispatch.toClient('S_EXIT', 1, {
-//			unk1: 0,
-//			unk2: 16
-		})		
+		// The servers sends the S_EXIT packet with the int64 data:
+		// 00000000 10000000
+		// Not sure of the purpose, but client seems fine without it.
+		dispatch.toClient('S_EXIT', 1, {})		
 	});	
 	
 }
