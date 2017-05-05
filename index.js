9743b01203d2ab3c526d12a70ebd7ac15aaac1dc
teralove <terascripts@gmail.com>
2017-05-05 14:07:42 -0400

    removed some spaces

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //vers 1.0
 
 module.exports = function ExitInstantly(dispatch) {
-				
-    dispatch.hook('S_PREPARE_EXIT', 1, function(event) {
+
+	dispatch.hook('S_PREPARE_EXIT', 1, function(event) {
 		// The servers sends the S_EXIT packet with the int64 data:
 		// 00000000 10000000
 		// Not sure of the purpose, but client seems fine without it.
 		dispatch.toClient('S_EXIT', 1, {})		
-	});	
-	
+	});
+
 }
