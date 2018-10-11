8438993e51f9f6585b8dd4e5a452dceeee577452
FichteFoll <fichtefoll2@googlemail.com>
2018-10-11 20:48:31 +0200

    Update C_EXIT def

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 module.exports = function ExitInstantly(dispatch) {
 
-	dispatch.hook('S_PREPARE_EXIT', 1, function(event) {
+	dispatch.hook('S_PREPARE_EXIT', 1, function() {
 		// The servers sends the S_EXIT packet with the int64 data:
 		// 00000000 10000000
 		// Not sure of the purpose, but client seems fine without it.
-		dispatch.toClient('S_EXIT', 1, {})		
+		dispatch.toClient('S_EXIT', 3, {})		
 	});
 
 }
