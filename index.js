2a899c13161a8e94fb1bfa224d93b5a113c9b1ea
teralove <terascripts@gmail.com>
2018-12-16 19:41:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = function ExitInstantly(mod) {
 
-	mod.hook('S_PREPARE_EXIT', 1, function(event) {
-		mod.send('S_EXIT', 1, {})		
-	});
+    mod.hook('S_PREPARE_EXIT', 1, event => {
+        mod.send('S_EXIT', 3, {
+            category: 0,
+            code: 0
+        });
+    });
 }
